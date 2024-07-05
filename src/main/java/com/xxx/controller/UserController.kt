534a5e9611a75ac8c4e5dd68dc166d597a41c26b
reigadegr <2722688642@qq.com>
2024-07-05 22:17:33 +0800

    换成@PathVariable

--- src/main/java/com/xxx/controller/UserController.kt
+++ src/main/java/com/xxx/controller/UserController.kt
@@ -3,22 +3,19 @@ package com.xxx.controller
 import com.xxx.pojo.User
 import com.xxx.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/uniselect")
+//@RequestMapping("/uniselect")
 class UserController {
     @Autowired
     private lateinit var userService: UserService
 
     @CrossOrigin(origins = ["*"])
-    @RequestMapping("/login")
-    fun findByUserIdAndPassword(@RequestBody user: User): User? {
-        val studentId = user.student_id ?: ""
-        val password = user.password ?: ""
+    @RequestMapping(value = ["/login/{studentId}/{password}"], method = [RequestMethod.GET])
+    fun findByUserIdAndPassword(@PathVariable studentId: String, @PathVariable password: String): User? {
+//        val studentId = user.student_id ?: ""
+//        val password = user.password ?: ""
         // println("学号：$studentId")
         // println("密码：$password")
         return userService.findByUserIdAndPassword(studentId, password)
